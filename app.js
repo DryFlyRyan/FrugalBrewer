4a8e6ef20dfd1399acbf473b60a2688be8d2a8be
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-15 15:18:47 -0700

    having trouble getting jquery to start

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('doc loading');
+
+$(document).ready(function() {
+  console.log('doc ready');
+  // $.get('http://api.brewerydb.com/v2/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
+  //   console.log('requested');
+  // }).done(function(data){
+  //   console.log(data);
+  // });
+
+});
