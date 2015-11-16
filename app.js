b290421fdf13f601c3a0670627b112fa76356394
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-16 09:36:14 -0700

    slow and steady progress. some initial styling for form

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ console.log('doc loading');
 
 $(document).ready(function() {
   console.log('doc ready');
-  // $.get('http://api.brewerydb.com/v2/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
-  //   console.log('requested');
-  // }).done(function(data){
-  //   console.log(data);
-  // });
+
+  var fermentables = JSON.parse(localStorage.getItem('fermentables'))
+
+  if (!fermentables) {
+    $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
+      console.log('requested');
+    }).done(function(data){
+      fermentables = localStorage.setItem('fermentables', JSON.stringify(data))
+      console.log(data);
+    });
+  }
+
+  console.log(fermentables);
+
+  // var hops = JSON.parse(localStorage.getItem())
+  //
+  // if (!hops) {
+  //   $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/hops/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
+  //     console.log('requested');
+  //   }).done(function(data){
+  //     hops = localStorage.setItem('hops', JSON.stringify(data))
+  //   });
+  // }
+
+
 
 });
