aebe9bf36709a06e32c704de7b8e48aa62a0e4df
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-16 11:09:24 -0700

    working on asynch requests. wrote a selfreferencing function to take place of loop

--- app.js
+++ app.js
@@ -6,15 +6,45 @@ $(document).ready(function() {
   var fermentables = JSON.parse(localStorage.getItem('fermentables'))
 
   if (!fermentables) {
+    var fermentList = '';
     $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
       console.log('requested');
     }).done(function(data){
-      fermentables = localStorage.setItem('fermentables', JSON.stringify(data))
-      console.log(data);
+      // fermentables = localStorage.setItem('fermentables', JSON.stringify(data));
+      fermentList = fermentList + JSON.stringify(data);
+      var pageNumber = data.currentPage;
+      var totalPages = data.numberOfPages;
+      console.log(totalPages);
+      if (pageNumber < totalPages) {
+        for (var i = pageNumber+1; i <= totalPages; i++) {
+          console.log(i);
+          $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb&p='+ (i).toString(), function () {
+            console.log('serverStatus');
+            console.log('requesting page', i);
+          }).done(function(data){
+            console.log('returning page', i);
+          });
+        }
+      }
     });
   }
 
-  console.log(fermentables);
+  var requestFunction = function(startPage, stopPage) {
+    var currentPage = startPage;
+    var lastPage = stopPage;
+    $.get($.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb&p='+ (currentPage).toString()).done(function(data) {
+      currentPage += 1
+      console.log(data);
+      if (currentPage <= lastPage) {
+        requestFunction(currentPage, lastPage);
+      }
+      else {
+        return false;
+      }
+    })
+  }
+
+
 
   // var hops = JSON.parse(localStorage.getItem())
   //
