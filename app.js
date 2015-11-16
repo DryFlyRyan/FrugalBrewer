e56de92f324a1c411a74f0d028c8ec5094260c41
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-16 15:35:54 -0700

    got api to push to local storage. parsed local storage data. appended filtered data to #fermentables-selector in DOM. Saved myself 1/2 hour of work entering in all options by spending a day and a half on this.

--- app.js
+++ app.js
@@ -2,37 +2,70 @@ console.log('doc loading');
 
 $(document).ready(function() {
   console.log('doc ready');
-  var fermentablesWorking = '';
-  var fermentables = localStorage.getItem('fermentables');
-  var parsedFermentables = null;
-  var hello = "hello world";
 
+  var workingData = [];
+  var fermentables = JSON.parse(localStorage.getItem('fermentables'));
+  var filteredFermentables = JSON.parse(localStorage.getItem('filteredFermentables'));
 
-  if (!fermentables) {
-    var fermentList = '';
-    $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
-      console.log('requested');
-    }).done(function(data){
-      console.log(data);
-      var pageNumber = data.currentPage;
-      var totalPages = data.numberOfPages;
-      localStorage.setItem('fermentables' + pageNumber.toString(), JSON.stringify(data));
-      if (pageNumber < totalPages) {
-        requestFunction(pageNumber+1, totalPages);
+  var filterFermentables = function() {
+    var filterFermentables = [];
+    for (var i = 0; i < fermentables.length; i++) {
+      if (fermentables[i].potential) {
+        filterFermentables = filterFermentables.concat(fermentables[i]);
       }
-    });
-  }
+    }
+    localStorage.setItem('filteredFermentables', JSON.stringify( filteredFermentables));
+  };
 
   var requestFunction = function(startPage, stopPage) {
     var currentPage = startPage;
     var lastPage = stopPage;
     $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb&p='+ (currentPage).toString()).done(function(data) {
-      localStorage.setItem('fermentables' + currentPage.toString(), JSON.stringify(data));
+      workingData = workingData.concat(data.data);
+      // console.log(workingData)
       currentPage += 1;
       if (currentPage <= lastPage) {
         requestFunction(currentPage, lastPage);
+      } else {
+        localStorage.setItem('fermentables', JSON.stringify(workingData));
+        // console.log(JSON.parse(localStorage.getItem('fermentables')));
       }
     });
   };
+
+  var fermentablesOptions = function () {
+    var target = $(document).find('#fermentables-selector').children('optgroup');
+    for(var i = 0; i < filteredFermentables.length; i++) {
+      var name = filteredFermentables[i].name;
+      console.log(name);
+      $(target).append('<option>' + name + '</option>');
+    }
+  };
+
+  if (!fermentables) {
+    $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
+    }).done(function(data){
+      var pageNumber = data.currentPage;
+      var totalPages = data.numberOfPages;
+      workingData = workingData.concat(data.data);
+      // console.log(data.data);
+      if (pageNumber < totalPages) {
+        requestFunction(pageNumber+1, totalPages);
+      } else {
+        localStorage.setItem('fermentables', JSON.stringify(workingData));
+      }
+    });
+  } else {
+    console.log(JSON.parse(localStorage.getItem('fermentables')));
+    filterFermentables();
+    console.log(JSON.parse(localStorage.getItem('filteredFermentables')));
+    fermentablesOptions();
+  }
+
+
+
+
+
+
 
 });
