7c0b445d443994fbc9acfb19b756bd4d943f475d
Ryan Douglas <ryan@luckydogdigital.com>
2015-11-17 18:31:04 -0700

    formulas work, including search. pretty nifty

--- app.js
+++ app.js
@@ -3,48 +3,50 @@ console.log('doc loading');
 $(document).ready(function() {
   console.log('doc ready');
 
-  var workingData = [];
+  var workingDataFermentables = [];
   var fermentables = JSON.parse(localStorage.getItem('fermentables'));
   var fermentablesFiltered = JSON.parse(localStorage.getItem('fermentablesFiltered'));
 
+  var workingDataHops = [];
+  var hops = JSON.parse(localStorage.getItem('hops'));
+  var hopsFiltered = JSON.parse(localStorage.getItem('hopsFiltered'));
 
 
-  var requestFunction = function(startPage, stopPage, source) {
+  var requestFunction = function(startPage, stopPage, sourceArray, filteredArray, workingArray, filterCriteria, categoryName) {
 
     var currentPage = startPage;
     var lastPage = stopPage;
-    var destination = source;
+    var destination = categoryName;
     if (typeof destination !== 'string') {
-      console.log('requestFunction(source) must be a string');
-      destination = source.toString();
+      destination = categoryName.toString();
     }
+    console.log('loop ', currentPage);
+
     $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/' + destination + '/?key=166f0b6348fdccde864dc9aecb3d50bb&p='+ (currentPage).toString()).done(function(data) {
-      workingData = workingData.concat(data.data);
+      workingArray = workingArray.concat(data.data);
       // console.log(workingData)
       currentPage += 1;
       if (currentPage <= lastPage) {
-        requestFunction(currentPage, lastPage, destination);
+        requestFunction(currentPage, lastPage, sourceArray, filteredArray, workingArray, filterCriteria, categoryName);
       } else {
-        fermentables = workingData;
-        localStorage.setItem(destination, JSON.stringify(fermentables));
+        sourceArray = workingArray;
+        localStorage.setItem(destination, JSON.stringify(sourceArray));
         // console.log(JSON.parse(localStorage.getItem('fermentables')));
-        filterData(fermentables, fermentablesFiltered, "potential", 'fermentables');
+        filterData(sourceArray, filteredArray, filterCriteria, categoryName);
       }
     });
   };
 
   var filterData = function(sourceArray, filteredArray, filterCriteria, categoryName) {
     console.log("filterData called");
-    // var criteria = JSON.parse(filterCriteria);
     var workingArray = [];
     for (var i = 0; i < sourceArray.length; i++) {
-      var target = JSON.parse("[{sourceArray[i]." + filterCriteria + "}]")
-      if (target) {
+      if (sourceArray[i].hasOwnProperty(filterCriteria)) {
         workingArray = workingArray.concat(sourceArray[i]);
       }
     }
     filteredArray = workingArray;
-    localStorage.setItem(categoryName + 'Filtered', JSON.stringify(workingArray));
+    localStorage.setItem(categoryName + 'Filtered', JSON.stringify(filteredArray));
     optionsAppender(filteredArray, categoryName);
   };
 
@@ -58,21 +60,47 @@ $(document).ready(function() {
   };
 
   if (!fermentables) {
+    console.log('loop 1');
     $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/fermentables/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
     }).done(function(data){
+      console.log('data delivered!');
       var pageNumber = data.currentPage;
       var totalPages = data.numberOfPages;
-      workingData = workingData.concat(data.data);
+      console.log("pageNumber = ", pageNumber);
+      workingDataFermentables = workingDataFermentables.concat(data.data);
       // console.log(data.data);
       if (pageNumber < totalPages) {
-        requestFunction(pageNumber+1, totalPages, 'fermentables');
+        requestFunction(pageNumber+1, totalPages, fermentables, fermentablesFiltered, workingDataFermentables, 'potential', 'fermentables');
       }
     });
+  } else {
+    optionsAppender(fermentables, 'fermentables');
   }
 
-  if (fermentablesFiltered) {
-    optionsAppender();
+  if (!hops) {
+    console.log('loop 1');
+    $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/hops/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
+    }).done(function(data){
+      console.log(data);
+      console.log('data delivered!');
+      var pageNumber = data.currentPage;
+      var totalPages = data.numberOfPages;
+      console.log("pageNumber = ", pageNumber);
+      workingDataHops = workingDataHops.concat(data.data);
+      // console.log(data.data);
+      if (pageNumber < totalPages) {
+        requestFunction(pageNumber+1, totalPages, hops, hopsFiltered, workingDataHops, 'alphaAcidMin', 'hops');
+      }
+    });
+  } else {
+    optionsAppender(hops, 'hops');
   }
+
+  // $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/hops/?key=166f0b6348fdccde864dc9aecb3d50bb', function() {
+  // }).done(function(data){
+  //   console.log(data);
+  // });
+
 
 
 
